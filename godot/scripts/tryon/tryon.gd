af7f44a2664e8c194eb4415d029633775138029f
NeoNetz <aksafadillah@gmail.com>
2025-11-03 13:06:38 +0700

    Update main menu

--- godot/scripts/tryon/tryon.gd
+++ godot/scripts/tryon/tryon.gd
@@ -6,8 +6,6 @@ var click_sound: AudioStreamPlayer
 
 
 func _ready() -> void:
-	$Fade_transition/AnimationPlayer.play("fade_out")
-	
 	# Load audio resources
 	hover_sound = AudioStreamPlayer.new()
 	hover_sound.stream = load("res://godot/assets/sounds/HoverButton.mp3")
@@ -27,13 +25,13 @@ func setup_button_sounds() -> void:
 	"""Detect and setup sounds for all TextureButton inside Button_manager"""
 	var button_manager = get_node_or_null("Button_manager")
 	if button_manager == null:
-		print("[About] Button_manager not found!")
+		print("[TryOn] Button_manager not found!")
 		return
 	
 	# Find all TextureButton children
 	var buttons = find_texture_buttons(button_manager)
 	
-	print("[About] Found %d TextureButton(s) in Button_manager" % buttons.size())
+	print("[TryOn] Found %d TextureButton(s) in Button_manager" % buttons.size())
 	
 	for button in buttons:
 		if button is TextureButton:
@@ -47,7 +45,7 @@ func find_texture_buttons(node: Node) -> Array:
 	for child in node.get_children():
 		if child is TextureButton:
 			buttons.append(child)
-			print("[About] Found TextureButton: %s" % child.name)
+			print("[TryOn] Found TextureButton: %s" % child.name)
 		
 		# Recursive search in children
 		if child.get_child_count() > 0:
@@ -61,40 +59,40 @@ func connect_button_signals(button: TextureButton) -> void:
 	# Connect mouse_entered for hover sound
 	if not button.mouse_entered.is_connected(_on_button_hover):
 		button.mouse_entered.connect(_on_button_hover.bind(button))
-		print("[About] Connected hover sound to: %s" % button.name)
+		print("[TryOn] Connected hover sound to: %s" % button.name)
 	
 	# Connect pressed for click sound
 	if not button.pressed.is_connected(_on_button_click):
 		button.pressed.connect(_on_button_click.bind(button))
-		print("[About] Connected click sound to: %s" % button.name)
+		print("[TryOn] Connected click sound to: %s" % button.name)
 	
 	# Connect specific button actions
 	if button.name == "Back":
 		if not button.pressed.is_connected(_on_back_pressed):
 			button.pressed.connect(_on_back_pressed)
-			print("[About] Connected Back button to MainMenu transition")
+			print("[TryOn] Connected Back button to MainMenu transition")
 
 
 func _on_button_hover(button: TextureButton) -> void:
 	"""Play hover sound when mouse enters button"""
-	print("[About] Hover on button: %s" % button.name)
+	print("[TryOn] Hover on button: %s" % button.name)
 	if hover_sound and hover_sound.stream:
 		hover_sound.play()
 
 
 func _on_button_click(button: TextureButton) -> void:
 	"""Play click sound when button is pressed"""
-	print("[About] Click on button: %s" % button.name)
+	print("[TryOn] Click on button: %s" % button.name)
 	if click_sound and click_sound.stream:
 		click_sound.play()
 
 
 func _on_back_pressed() -> void:
 	"""Handle Back button press - return to MainMenu"""
-	print("[About] Back button pressed - returning to MainMenu")
+	print("[TryOn] Back button pressed - returning to MainMenu")
 	
-	# Play fade in animation
-	var anim_player = $Fade_transition/AnimationPlayer
+	# Play fade in animation (if exists)
+	var anim_player = get_node_or_null("Fade_transition/AnimationPlayer")
 	if anim_player:
 		anim_player.play("fade_in")
 		# Wait for animation to finish
